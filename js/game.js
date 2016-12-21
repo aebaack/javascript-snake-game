d62e6d6ec67ce3fe49d46403d223d1009fae3780
Aidan Baack <aebaack@gmail.com>
2016-12-20 19:58:01 -0700

    Create basic createRow and createColumns functions

--- js/game.js
+++ js/game.js
@@ -4,8 +4,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const grid = document.getElementById('grid-container');
   const height = grid.offsetHeight;
   const width = grid.offsetWidth;
+  let row = createRow('a');
+  createColumns(row);
+  grid.appendChild(row);
 });
 
 function createRow(height) {
-  let newRow = document.createElement("div");
+  let newRow = document.createElement('div');
+  newRow.style.height = '15px';
+  newRow.style.width = '100%';
+  return newRow;
+}
+
+function createColumns(row) {
+  let newCol = document.createElement('div');
+  newCol.style.height = 'inherit';
+  newCol.style.width = '15px';
+  row.appendChild(newCol);
 }
