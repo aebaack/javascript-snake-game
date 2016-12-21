c5144090dba51fc30ae6b9eb11952bf614161b62
Aidan Baack <aebaack@gmail.com>
2016-12-20 19:54:12 -0700

    Grab grid-container height and width with js and start createRow function

--- js/game.js
+++ js/game.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const grid = document.getElementById('grid-container');
+  const height = grid.offsetHeight;
+  const width = grid.offsetWidth;
+});
+
+function createRow(height) {
+  let newRow = document.createElement("div");
+}
