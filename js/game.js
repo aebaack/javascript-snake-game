dc05d5573748e60a211df7c1225096cca980729e
Aidan Baack <aebaack@gmail.com>
2016-12-21 20:44:26 -0700

    Add row and column id to squares and create a basic game loop

--- js/game.js
+++ js/game.js
@@ -4,6 +4,7 @@ const squareDimensions = '23';
 
 document.addEventListener('DOMContentLoaded', (event) => {
   createGrid();
+  //gameLoop();
 });
 
 function createGrid() {
@@ -15,29 +16,37 @@ function createGrid() {
   grid.style.height = `${numRows * squareDimensions}px`;
 
   for (let i = 0; i < numRows; i++) {
-    let row = createRow(numCols);
+    let row = createRow(numCols, i);
     grid.appendChild(row);
   }
 }
 
-function createRow(numCols) {
+function createRow(numCols, currentRow) {
   let newRow = document.createElement('div');
 
   newRow.className = 'row';
   newRow.style.height = `${squareDimensions}px`;
 
-  createColumns(newRow, numCols);
+  createColumns(newRow, numCols, currentRow);
 
   return newRow;
 }
 
-function createColumns(row, numCols) {
+function createColumns(row, numCols, currentRow) {
   for (let i = 0; i < numCols; i++) {
     let newCol = document.createElement('div');
+
     newCol.className = 'col';
+    newCol.id = `${currentRow}_${i}`;
+
     newCol.style.width = `${squareDimensions}px`;
     newCol.style.height = '100%';
 
     row.appendChild(newCol);
   }
+}
+
+function gameLoop() {
+  console.log('this ran');
+  setTimeout(gameLoop, 1000);
 }
