2ef42400a7dd8f770f9206b61686dd3974475b35
Aidan Baack <aebaack@gmail.com>
2016-12-21 18:12:34 -0700

    Create row and columns with set pixel dimensions for simplicity

--- js/game.js
+++ js/game.js
@@ -1,24 +1,39 @@
 'use strict';
 
+const squareDimensions = '23';
+
 document.addEventListener('DOMContentLoaded', (event) => {
-  const grid = document.getElementById('grid-container');
-  const height = grid.offsetHeight;
-  const width = grid.offsetWidth;
-  let row = createRow('a');
-  createColumns(row);
-  grid.appendChild(row);
+  createGrid();
 });
 
-function createRow(height) {
+function createGrid() {
+  let grid = document.getElementById('grid-container');
+
+  const numCols = Math.floor(grid.offsetWidth / squareDimensions);
+  grid.style.width = `${numCols * squareDimensions}px`;
+
+  let row = createRow(numCols);
+  grid.appendChild(row);
+}
+
+function createRow(numCols) {
   let newRow = document.createElement('div');
-  newRow.style.height = '15px';
-  newRow.style.width = '100%';
+
+  newRow.className = 'row';
+  newRow.style.height = `${squareDimensions}px`;
+
+  createColumns(newRow, numCols);
+
   return newRow;
 }
 
-function createColumns(row) {
-  let newCol = document.createElement('div');
-  newCol.style.height = 'inherit';
-  newCol.style.width = '15px';
-  row.appendChild(newCol);
+function createColumns(row, numCols) {
+  for (let i = 0; i < numCols; i++) {
+    let newCol = document.createElement('div');
+    newCol.className = 'col';
+    newCol.style.width = `${squareDimensions}px`;
+    newCol.style.height = '100%';
+
+    row.appendChild(newCol);
+  }
 }
